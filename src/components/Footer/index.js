198f94d6ed711eb476ffc04bab2cfd273167fe08
Jasmine <jasmine.kim.18@gmail.com>
2019-12-31 23:40:25 -0800

    Footer skeleton in place

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles.scss";
+
+
+const Footer = () => {
+    return(
+        <div class = "footer-container">
+            <a className = "footer-link" href = "_blank">
+                About
+            </a>
+            <a className = "footer-link" href = "_blank">
+                Contact
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 import "./styles.scss";
 import Header from '../Header';
+import Footer from '../Footer';
 
 const Landing = () => {
     return(
         <div class = "landing">
             <Header/>
+            <Footer/>
         </div>
     )
 }
