e51005fcb0c7bbd210deaf3614fa3d1018714c09
Jasmine <jasmine.kim.18@gmail.com>
2020-01-03 22:35:24 -0800

    Calculating euclidean distance from target makeup

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,6 +1,9 @@
+# Euclidean distance measurement
+# https://stackoverflow.com/questions/54242194/python-find-the-closest-color-to-a-color-from-giving-list-of-colors
 import flask
 from flask import request
 import requests 
+from math import sqrt
 import pandas as pd
 
 app = flask.Flask("__main__")
@@ -15,15 +18,34 @@ def get_query_from_react():
     data = request.get_json()
     makeup =  data.get('data', {}).get('query')[0]
     color =  data.get('data', {}).get('query')[1]
-    print makeup, color ## Retrieved user input
 
-    r = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + makeup).content
+    h = color.lstrip('#')
+    color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+    r = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + 'nail_polish').content
     
-    #print r
-    
+    # move json data into pandas dataframe
+
     df = pd.read_json(r)
-    print df
+    #print df['id']
+    df['product_colors'].values
 
+    found = []
+   # iterate through each hex value
+    for i in df['product_colors']:
+        if i:
+            for j in i:
+                k = j['hex_value'].lstrip('#')
+                # calc distance between color and j['hex_value]
+                temp = tuple(int(k[i:i+2], 16) for i in (0, 2, 4))
+                r, g, b = color
+                cr, cg, cb = temp
+                #print r,g,b
+                #print cr,cg,cb
+                color_diff = sqrt(abs(r - cr)**2 + abs(g - cg)**2 + abs(b - cb)**2)
+                print j['hex_value'], color_diff
+                #found = found + distance
+    print found
     return data
 
 
