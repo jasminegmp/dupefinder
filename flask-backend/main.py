ac6c738cdea7a2fd5f4bc3dfd515deee605303fc
Jasmine <jasmine.kim.18@gmail.com>
2020-01-05 13:15:28 -0800

    Color distance measurement sorting complete

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,13 +1,13 @@
-# Euclidean distance measurement
-# https://stackoverflow.com/questions/54242194/python-find-the-closest-color-to-a-color-from-giving-list-of-colors
 import flask
 from flask import request
 import requests 
 from math import sqrt
+import numpy as np
 import pandas as pd
 
 app = flask.Flask("__main__")
 @app.route("/")
+
 
 def my_index():
     return flask.render_template("index.html", token="Hello Flaks+react!")
@@ -20,32 +20,36 @@ def get_query_from_react():
     color =  data.get('data', {}).get('query')[1]
 
     h = color.lstrip('#')
-    color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+    selected_color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
-    r = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + 'nail_polish').content
+    r = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + makeup).content
+    print ('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + makeup)
     
     # move json data into pandas dataframe
+    mydf = pd.read_json(r)
 
-    df = pd.read_json(r)
-    #print df['id']
-    df['product_colors'].values
+    # Learned how to flatten dataframe here:
+    # https://stackoverflow.com/questions/54537616/flatten-dataframe-nested-list-array-with-extra-index-keys-for-time-series
+    df = pd.concat({k: pd.DataFrame(array) for k, array in mydf.pop('product_colors').items()})
+    df = df.reset_index(level=1, drop=True).join(mydf).reset_index(drop=True)
 
-    found = []
-   # iterate through each hex value
-    for i in df['product_colors']:
-        if i:
-            for j in i:
-                k = j['hex_value'].lstrip('#')
-                # calc distance between color and j['hex_value]
-                temp = tuple(int(k[i:i+2], 16) for i in (0, 2, 4))
-                r, g, b = color
-                cr, cg, cb = temp
-                #print r,g,b
-                #print cr,cg,cb
-                color_diff = sqrt(abs(r - cr)**2 + abs(g - cg)**2 + abs(b - cb)**2)
-                print j['hex_value'], color_diff
-                #found = found + distance
-    print found
+    distances = []
+    for hex_color in df['hex_value']:
+        #print hex_color
+        hex_color = hex_color.lstrip('#')
+        current_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+        r, g, b = selected_color
+        cr, cg, cb = current_color
+
+        # Euclidean distance measurement
+        # https://stackoverflow.com/questions/54242194/python-find-the-closest-color-to-a-color-from-giving-list-of-colors
+        color_diff = sqrt(abs(r - cr)**2 + abs(g - cg)**2 + abs(b - cb)**2)
+        
+        distances.append(color_diff)
+    df['distances'] = distances
+    df =df.sort_values(by=['distances'])
+    df.to_csv(r'dataframe_df.csv', index=False, encoding='utf-8')
+
     return data
 
 
