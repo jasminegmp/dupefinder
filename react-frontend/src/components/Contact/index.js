ff0944b6c5a2241ee5edf309d38951aab60c9ff7
Jasmine <jasmine.kim.18@gmail.com>
2020-01-07 13:41:52 -0800

    Minor typo

--- react-frontend/src/components/Contact/index.js
+++ react-frontend/src/components/Contact/index.js
@@ -25,8 +25,7 @@ class ContactBase extends React.Component{
           <div class = "text-container"> 
             <h1>Contact</h1>
             <p>Developed by Jasmine Gump, 2020</p>
-            <p>Check out my website for other work at <a href="jasminegump.com" target = "_blank" rel="noopener noreferrer">jasminegump.com</a></p>
-            <p>Email me at <a href="mailto:jasminegmp@gmail.com" target = "_blank" rel="noopener noreferrer">jasminegmp@gmail.com</a></p>
+            <p>Find me at <a href="https://jasminegump.com" target = "_blank" rel="noopener noreferrer">jasminegump.com</a></p>
           </div>
           <div class = "box"><Footer/></div>
         </div>
